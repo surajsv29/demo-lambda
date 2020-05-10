e1afbdd46f3f76497e830598b3ff6093c5e67a27
suraj <suraj.sv29@gmail.com>
2020-05-10 14:37:57 +0530

    New Repo

---
+++ deleteUser/index.js
@@ -0,0 +1,22 @@
+const sql = require('mysql')
+exports.handler = (event, context, callback)=>{
+const connection = sql.createConnection({
+    host: 'ch-buyogo-rds-ch-dev1-rdscluster-1dmi9tf2dmuce.cluster-ctgjyfgcje6u.eu-central-1.rds.amazonaws.com',
+    user: 'masteruser',
+    password : '1XwStHpav01mVUeWnoYqC469SeIlMWxAuQ2',
+    database: 'buyogo'
+})
+
+connection.connect()
+
+ connection.query('delete from demoUser where id=?',[event.pathParameters.userId],(error, results,field)=>{
+            if(!error){
+            callback(null, {"statusCode": 200, "body": JSON.stringify(results)})
+            connection.end()
+            }
+            else{
+            callback(error,null)
+            connection.end()
+            }
+        })
+}
