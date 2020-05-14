2b81d7504325d472fff9a6f8484dd8fa275bb24f
Suraj <suraj.sv29@gmail.com>
2020-05-14 23:07:06 +0530

    Some comments on how callback works.

--- doodle.js
+++ doodle.js
@@ -4,6 +4,13 @@ const poorFunction = ()=>{
     console.log('Say Hello to poor function :(');
 }
 
+setTimeout(()=>{
+    console.log('In timeout');
+},2000)
+//So this generally says, the mechanism of timeout function, that you give to parameters :
+// 1. CallBack function :- "whatever" you wanna do. Obviously, this function might be calling somehwere in tiemout code as -- callback();
+// 2. Time(ms) : After what time you wanna do that "whatever."
+
 const richFunction = callback => {
     console.log('I am rich function. See poorFunc below me');
     callback();// We are simply executing the function here.
